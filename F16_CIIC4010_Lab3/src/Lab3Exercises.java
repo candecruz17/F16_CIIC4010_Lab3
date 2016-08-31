fa2516d422f3f20acbd9e71f5865a743ccb17110
4015c4 <4015c4@guayaba.ece.uprm.edu>
2016-08-31 12:23:03 -0400

    Changes made:
    A setTitle method was added
    The setSize was modified

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,11 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		JFrame myFrame = new JFrame("Hello, World!");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setTitle("Rafael Cruz");
 		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
-}
+} 
