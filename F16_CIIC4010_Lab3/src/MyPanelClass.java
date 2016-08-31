88cb60a81c2bafc0b7f370a8a2c441091d6b8106
4015c4 <4015c4@guayaba.ece.uprm.edu>
2016-08-31 13:06:23 -0400

    changes made:
    drawOval was replaced by a fillOval
    location of oval is now in center of frame

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -20,17 +20,30 @@ public class MyPanelClass extends JPanel {
         int y2 = getHeight() - myInsets.bottom - 1;
         int width = x2 - x1;
         int height = y2 - y1;
+        int value = (height - 55)/2;
+        int value1 = (width - 55)/2;
  
         //Paint the background
         g.setColor(Color.BLACK);
         g.fillRect(x1, y1, width + 1, height + 1);
         
-        //Draw a border
-        g.setColor(Color.WHITE);
-        g.drawRect(x1, y1, width, height);
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillOval(value1, value, 55, 55);
         
-        //Draw second border
-        g.setColor(Color.GREEN);
-        g.drawRect(x1 + 10, y1 + 10, width - 20, height - 20);
+//        //Draw a border
+//        g.setColor(Color.WHITE);
+//        g.drawRect(x1, y1, width, height);
+//        
+//        //Draw second border
+//        g.setColor(Color.GREEN);
+//        g.drawRect(x1 + 10, y1 + 10, width - 20, height - 20);
+//        
+//        //Draw a cross line
+//        g.setColor(Color.WHITE);
+//        g.drawLine(x1, y1, x2, y2);
+//        
+//        //Draw a second cross line
+//        g.setColor(Color.RED);
+//        g.drawLine(x2, y1,  x1, y2);
 	}
 }
